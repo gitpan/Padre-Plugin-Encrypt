e2c19973daa48fbb28f9ba0212fb7656aa936d74
أحمد محمد زواوي - Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2010-10-29 07:31:23 +0000

    Import of AZAWAWI/Padre-Plugin-Encrypt-0.11 from CPAN.
    gitpan-cpan-distribution: Padre-Plugin-Encrypt
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         AZAWAWI/Padre-Plugin-Encrypt-0.11.tar.gz
    gitpan-cpan-author:       AZAWAWI
    gitpan-cpan-maturity:     released

--- lib/Padre/Plugin/Encrypt.pm
+++ lib/Padre/Plugin/Encrypt.pm
@@ -1,6 +1,6 @@
 package Padre::Plugin::Encrypt;
 BEGIN {
-  $Padre::Plugin::Encrypt::VERSION = '0.10';
+  $Padre::Plugin::Encrypt::VERSION = '0.11';
 }
 
 # ABSTRACT: Encrypt/decrypt files in Padre
@@ -133,7 +133,7 @@ Padre::Plugin::Encrypt - Encrypt/decrypt files in Padre
 
 =head1 VERSION
 
-version 0.10
+version 0.11
 
 =head1 SYNOPSIS
 
